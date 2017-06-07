d968e92053c7b580f08efd52687f0fa29a312f26
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-07 11:31:29 +0100

    Able to increase and decrease temperature. tests passing.

--- thermostat/jasmine-standalone-2.6.2/spec/ThermostatSpec.js
+++ thermostat/jasmine-standalone-2.6.2/spec/ThermostatSpec.js
@@ -6,7 +6,18 @@ describe("Thermostat", function() {
   beforeEach(function(){
     thermostat = new Thermostat();
   });
+
   it("should start at 20 degrees", function() {
       expect(thermostat.temperature).toEqual(20);
     });
+
+  it("should increase temperature by 1", function() {
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("should decrease temperature by 1", function() {
+    thermostat.down();
+    expect(thermostat.temperature).toEqual(19);
+  });
   });
